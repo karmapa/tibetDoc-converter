17b2b290d4b24a2d3e6799bb3a820a9c0824af11
rickie120243 <rickie120243@gmail.com>
2016-09-19 14:05:19 +0800

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+
+  devtool: 'eval',
+
+  entry: {
+    html: './src/index.html'
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  plugins: [
+    new webpack.NoErrorsPlugin()
+  ],
+
+  resolve: {
+    extensions: ['', '.js']
+  },
+
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        configFile: './.eslintrc',
+        exclude: /node_modules/
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel'],
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader'},
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]",
+      }
+    ]
+  }
+
+};
