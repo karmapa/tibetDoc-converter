7f3260101a2e44847c7f4875a8bc5afe56de8767
rickie120243 <rickie120243@gmail.com>
2016-09-19 16:23:51 +0800

    fix yoda if

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -49,7 +49,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
+  if (null === mainWindow) {
     createWindow();
   }
 });
