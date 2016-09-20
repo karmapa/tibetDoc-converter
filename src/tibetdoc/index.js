9268cb01b7606e22cdec89e505cb1de3c2b9013d
rickie120243 <rickie120243@gmail.com>
2016-09-20 15:39:05 +0800

    add semicolon

--- src/tibetdoc/index.js
+++ src/tibetdoc/index.js
@@ -1,20 +1,20 @@
 var r = new FileReader();
 function act() {
-  var file = fileInput.files[0]
+  var file = fileInput.files[0];
   r.onload = function() {
-    var data = r.result
-    data = TibetDoc.Parse(data.toString())
-    data = TibetDoc.JSONToHTML(data)
-    a.innerHTML = data
-    data = '<meta charset=utf-8>\n' + data
+    var data = r.result;
+    data = TibetDoc.Parse(data.toString());
+    data = TibetDoc.JSONToHTML(data);
+    a.innerHTML = data;
+    data = '<meta charset=utf-8>\n' + data;
     var b = new Blob([data], { encoding:'utf-8',type:'text/html' });
     console.log('blob', JSON.stringify(b))
     var url = URL.createObjectURL(b);
-    downLink.href = url
-    downLink.target = '_blank'
-    downLink.innerHTML = 'Download'
-    downLink.download = 'convertedDocument.txt'
-    document.getElementById('reload').style.display = 'inline'
+    downLink.href = url;
+    downLink.target = '_blank';
+    downLink.innerHTML = 'Download';
+    downLink.download = 'convertedDocument.txt';
+    document.getElementById('reload').style.display = 'inline';
   }
   r.readAsBinaryString(file); 
 }
