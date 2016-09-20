7621ba2de1d38680f85ac8ee9fc8d0bb71aabb43
rickie120243 <rickie120243@gmail.com>
2016-09-20 18:03:46 +0800

    fix dom method

--- src/tibetdoc/index.js
+++ src/tibetdoc/index.js
@@ -2,26 +2,27 @@ var handleConvert = document.getElementById('fileInput');
 handleConvert.addEventListener('change', converter);
 var r = new FileReader();
 function converter() {
-  var file = fileInput.files[0];
+  var file = document.getElementById('fileInput').files[0];
   r.onload = function() {
     var data = r.result;
     data = TibetDoc.Parse(data.toString());
     data = TibetDoc.JSONToHTML(data);
-    a.innerHTML = data;
+    document.getElementById('a').innerHTML = data;
     data = '<meta charset=utf-8>\n' + data;
     var b = new Blob([data], { encoding:'utf-8',type:'text/html' });
-    console.log('blob', JSON.stringify(b))
+    console.log('blob', JSON.stringify(b));
     var url = URL.createObjectURL(b);
+    var downLink = document.getElementById('downLink');
     downLink.href = url;
     downLink.target = '_blank';
     downLink.innerHTML = 'Download';
     downLink.download = 'convertedDocument.txt';
     document.getElementById('reload').style.display = 'inline';
-  }
-  r.readAsBinaryString(file); 
+  };
+  r.readAsBinaryString(file);
 }
 
-reload.addEventListener('click', doReload);
+document.getElementById('reload').addEventListener('click', doReload);
 
 function doReload(event) {
   location.reload();
