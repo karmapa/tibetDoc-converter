7b24d8794cdb8f1e5d15821c10184311198fdcc7
rickie120243 <rickie120243@gmail.com>
2016-10-05 16:19:53 +0800

    fix packed app name

--- bin/build.sh
+++ bin/build.sh
@@ -4,4 +4,4 @@ cd dist
 sed -i '' 's/http:\/\/localhost:8080/\./g' index.html
 git clone git@github.com:rickie120243/pack-electron.git
 cp -r ./pack-electron/* ./
-electron-packager . tibetdoc-converter-v0.0.1 --platform=darwin,win32 --arch=x64 --overwrite
+electron-packager . tibetDoc-converter-v0.0.1 --platform=darwin,win32 --arch=x64 --overwrite
