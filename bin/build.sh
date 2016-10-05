9faee7361de0032da5a86e8f151ab54f2b47d0ec
rickie120243 <rickie120243@gmail.com>
2016-10-05 16:24:09 +0800

    add command to remove old app

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,7 @@
 
 cd dist
 sed -i '' 's/http:\/\/localhost:8080/\./g' index.html
+rm -rf tibetDoc-converter-*
 git clone git@github.com:rickie120243/pack-electron.git
 cp -r ./pack-electron/* ./
 electron-packager . tibetDoc-converter-v0.0.1 --platform=darwin,win32 --arch=x64 --overwrite
