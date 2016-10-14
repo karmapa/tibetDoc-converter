7793e3bb103822a6ae49c354dd2f32a57f298d6d
rickie120243 <rickie120243@gmail.com>
2016-10-14 15:32:49 +0800

    add version & zip name

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+ELECTRON_VERSION=1.3.4
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
+ZIP_IOS="tibetDoc-converter-darwin-x64-v${PACKAGE_VERSION}.zip"
+ZIP_WIN="tibetDoc-converter-win32-x64-v${PACKAGE_VERSION}.zip"
+
+rm -rf dist
 webpack
 cd dist
 sed -i '' 's/http:\/\/localhost:8080/\./g' index.html
 rm -rf tibetDoc-converter-*
 git clone git@github.com:rickie120243/pack-electron.git
 cp -r ./pack-electron/* ./
-electron-packager . tibetDoc-converter-v0.0.1 --platform=darwin,win32 --arch=x64 --overwrite
+electron-packager . tibetDoc-converter --platform=darwin,win32 --arch=x64 --overwrite
