d70cf7808a3ab7c725e48ae2bf682a193330a789
rickie120243 <rickie120243@gmail.com>
2016-11-03 17:11:04 +0800

    remove check if not empty

--- __tests__/DOM-test.js
+++ __tests__/DOM-test.js
@@ -10,14 +10,6 @@ it('Check input tag if file is undefined', () => {
   expect(checkApp.find('input').simulate('change', {target: {files: [undefined]}}).hasClass('downLink')).toEqual(false);
 });
 
-it('Check input tag if file is not empty', () => {
-  const checkApp = mount(
-    <App />
-  );
-  checkApp.find('input').simulate('change', {target: {files: ['abc']}});
-  expect(checkApp.hasClass('lineBreak')).toEqual(true);
-});
-
 it('Check if node exist', () => {
   const checkApp = shallow(
     <App />
