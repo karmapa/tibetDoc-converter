b79e4166337d8aba465fbe74b9313cdc1a2ed11a
rickie120243 <rickie120243@gmail.com>
2016-11-03 10:38:31 +0800

    fix event onchange > change

--- __tests__/DOM-test.js
+++ __tests__/DOM-test.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 import App from '../src/containers/App.js';
 
-it('Check input tag if file is empty', () => {
-  const checkApp = shallow(
+it('Check input tag if file is undefined', () => {
+  const checkApp = mount(
     <App />
   );
-  expect(checkApp.find('input').simulate('onchange', '').hasClass('lineBreak')).toEqual(false);
+  expect(checkApp.find('input').simulate('change', {target: {files: [undefined]}}).hasClass('empty')).toEqual(true);
+});
+
+it('Check input tag if file is not empty', () => {
+  const checkApp = mount(
+    <App />
+  );
+  checkApp.find('input').simulate('change', {target: {files: ['abc']}});
+  expect(checkApp.hasClass('lineBreak')).toEqual(true);
 });
 
 it('Check if node exist', () => {
